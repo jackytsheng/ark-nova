e2037d30be1559503ffd2c92a4585d4b8c647947
jackytsheng <jackytsheng@gmail.com>
2022-12-06 17:53:41 +1100

    [Data] working on curating data for the game

---
+++ tmp.py
@@ -0,0 +1,31 @@
+import json
+  
+# Opening JSON file
+f = open('src/jsons/animals.json')
+
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+new_list = []
+for obj in data:
+    
+    # Bonuses
+    obj['bonuses'] = "0/0/0" if obj['bonuses'] == "" else obj['bonuses'] 
+    a = obj['bonuses'].split("/")
+    print(a)
+    obj['bonuses'] = {"appeal": a[0], "conservation":a[1],"reputation":a[2]}
+
+		#	  
+  
+  
+    new_list.append(obj)
+
+  
+# Closing file
+f.close()
+
+with open('animal_data.json', 'w') as outfile:
+    json.dump(data, outfile)
