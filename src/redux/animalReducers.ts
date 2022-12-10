3197ddf695dacae6321d932df249af23244135ca
jackytsheng <jackytsheng@gmail.com>
2022-12-11 01:29:16 +1100

    [Game Logic] Added Icon when animal is played

--- src/redux/animalReducers.ts
+++ src/redux/animalReducers.ts
@@ -3,12 +3,13 @@ import { AnimalCard, Player } from '../GameType';
 
 const placeAnimal: CaseReducer<Player, PayloadAction<AnimalCard>> = (
   state,
-  { payload: { bonuses, cost } }
+  { payload: { bonuses, cost, type } }
 ) => {
   state.money -= cost;
   state.appeal += bonuses.appeal;
   state.conservation += bonuses.conservation;
   state.reputation += bonuses.reputation;
+  state.icons.push(type);
 };
 
 export default { placeAnimal };
